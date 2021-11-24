9b2921db43bf82d4330198da31c4f682f4e92f5b
Shankhadeep Das <shankhascm96@gmail.com>
2021-11-25 01:03:56 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const httpProxy = require('http-proxy');
+const finalhandler = require('finalhandler')
+const morgan = require('morgan')
+const fs = require('fs')
+const args = require('minimist')(process.argv.slice(2))
+
+let target = null
+let localPort = null
+let tlsKey = null
+let tlsCert = null
+
+let configFilePath = args['c'] ? args['c'] : args['config']
+configFilePath = configFilePath ? configFilePath : './https-port-forward.json'
+const configuration = fs.existsSync(configFilePath) ? JSON.parse(fs.readFileSync(configFilePath, 'utf8')) : null
+
+if (configuration) {
+    target = configuration.target ? configuration.target : null
+    localPort = configuration.localPort ? configuration.localPort : null
+    tlsKey = configuration.key ? configuration.key : null
+    tlsCert = configuration.cert ? configuration.cert : null
+}
+
+
+target = (args['t'] ? args['t'] : args['target']) ? (args['t'] ? args['t'] : args['target']) : target
+localPort = (args['p'] ? args['p'] : args['port']) ? (args['p'] ? args['p'] : args['port']) : localPort
+tlsKey = args['key'] ? args['key'] : tlsKey
+tlsCert = args['cert'] ? args['cert'] : tlsCert
+
+
+// TLS Setup
+const http_options = {
+    key: fs.existsSync(tlsKey) ? fs.readFileSync(tlsKey, 'utf8') : null,
+    cert: fs.existsSync(tlsCert) ? fs.readFileSync(tlsCert, 'utf8') : null
+}
+
+let http = null
+if (http_options.key && http_options.cert) {
+    http = require('https')
+} else {
+    http = require('http')
+}
+
+// Create the proxy server
+const proxy = httpProxy.createProxyServer({});
+
+// Setup Morgan
+const Logger = morgan('tiny')
+
+// Create the local server
+const server = http.createServer(http_options, function (req, res) {
+    try {
+        const done = finalhandler(req, res)
+        Logger(req, res, function (err) {
+            if (err) return done(err)
+            proxy.web(req, res, { changeOrigin: true, target });
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+server.listen(localPort)
+server.on('listening', () => {
+    console.log(
+        `Proxy server is listening for port ${localPort}\n`,
+        `Forwarded To: ${target}`
+    );
+})
